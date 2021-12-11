d1fb91ce2060d011087e2c25349543fa6125c171
BayronDavidMorales <93720978+BayronDavid@users.noreply.github.com>
2021-12-11 12:16:35 -0500

    Vida del token aumentada a un dia

--- wheelsApp/settings.py
+++ wheelsApp/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
 ]
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
@@ -72,7 +72,6 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
-
 }
 
 
